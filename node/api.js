43576e165ae15ba9231da4cc11945950c307f2fa
Noah Emke <noahremke@gmail.com>
2022-02-16 19:31:19 -0500

    test

--- node/api.js
+++ node/api.js
@@ -63,7 +63,7 @@ const server = http.createServer((req, res) => {
 	res.setHeader('Content-Type', 'text/html');
 
 	switch (req.url) {
-		// Docs page describing functions and usage
+		// Docs page Sdescribing functions and usage
 		case '/':
 			res.statusCode = 200;
 			res.end(docs);
@@ -88,14 +88,14 @@ const server = http.createServer((req, res) => {
 				if (req.headers['x-hub-signature'] == sig) {
 					res.statusCode = 200;
 					res.end();
-					exec('cd /var/www/factorio-library && git pull'); // Pull from github
+					console.log(exec('cd /var/www/factorio-library && git pull')); // Pull from github
 					// End the process so systemd restarts it with the new version pulled from git
-					process.kill(process.pid, 'SIGTERM'); 
+					//process.kill(process.pid, 'SIGTERM'); 
 				}
 			});
 
 			res.statusCode = 200;
-			res.end("Git pull attempted v2");
+			res.end("Git pull attempted v2.5");
 			break;
 
 		// Anything else is not a valid endpoint
